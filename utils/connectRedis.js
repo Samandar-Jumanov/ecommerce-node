f131fe78ff137932f355b65137dbe3afff36308b
Samandar-Jumanov <samandarjumanov055@gmail.com>
2023-12-17 05:29:39 -0800

    fixing bugs

--- utils/connectRedis.js
+++ utils/connectRedis.js
@@ -2,4 +2,12 @@ const redis = require('redis');
 require('dotenv').config();
 
 const redisClient = redis.createClient({url: process.env.REDIS_URL, legacyMode : true });
+(async function(){
+    try{
+      await redisClient.connect(); 
+      console.log("Redis connected ")
+    }catch{
+     throw new Error("Failed to connect redis", 500)
+    }
+})()
 module.exports = redisClient
