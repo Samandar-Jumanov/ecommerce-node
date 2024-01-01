69f8ccb389237cf49d8b19c1edde834e4deb8347
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-01 10:38:21 -0500

    fixing cors

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ app.use(morgan("dev"))
 
 app.use(cors({
     origin: 'http://localhost:3000/',
-    allowedHeaders: '*',
-    methods: 'GET, POST, PUT, DELETE',
-    credentials: true,
+    methods: ['GET', 'POST','PUT','DELETE'],
 }))
 
 //customer 
