5b78e310573ae5c4a7c372032cc8594c24707983
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-01 23:54:27 -0500

    updating code

--- admin/router.js
+++ admin/router.js
@@ -1,16 +1,18 @@
 const adminRouter = require('express').Router();
 const productCaseController = require('./productCase')
 const adminController = require('./admin-controller')
+const  {authenticateToken} =require('../utils/authToken');
+
 //admin 
 adminRouter.post('/auth/create-account/', adminController.createAccount)
 adminRouter.post('/auth/account-login/', adminController.loginAccount)
-adminRouter.post('/auth/logout/:adminId', adminController.Logout)
+adminRouter.post('/auth/logout/:adminId', authenticateToken ,  adminController.Logout)
 
 //product cases
-adminRouter.post('/product-case/create', productCaseController.createProductCase)
-adminRouter.delete('/product-case/delete/:adminId/:productCaseId', productCaseController.deleteProductCase)
-adminRouter.get('/product-cases/all' , productCaseController.getAllProductCases)
-adminRouter.put('/product-cases/update/:adminId/:productCaseId' , productCaseController.updateProductCase)
+adminRouter.post('/product-case/create',authenticateToken ,  productCaseController.createProductCase)
+adminRouter.delete('/product-case/delete/:adminId/:productCaseId', authenticateToken, productCaseController.deleteProductCase)
+adminRouter.get('/product-cases/all' ,authenticateToken , productCaseController.getAllProductCases)
+adminRouter.put('/product-cases/update/:adminId/:productCaseId',authenticateToken , productCaseController.updateProductCase)
 
 
 module.exports = adminRouter;
